517850f2e87ec11fc0816225c8d0406e13415079
tmalimage <tmalimage@gmail.com>
2017-05-01 21:56:14 +0800

    initial project setup

---
+++ src/main/java/com/itch/fmp/App.java
@@ -0,0 +1,17 @@
+package com.itch.fmp;
+
+import org.apache.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static Logger logger = Logger.getLogger(App.class.getName());
+ 
+    public static void main( String[] args )
+    {		
+		logger.info("Hello World");
+        System.out.println( "Hello World!" );
+    }
+}
