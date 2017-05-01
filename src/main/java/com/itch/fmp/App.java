6e52866d725128e20397568ab0712b44b3438b79
tmalimage <tmalimage@gmail.com>
2017-05-01 23:24:30 +0800

    schdule

--- src/main/java/com/itch/fmp/App.java
+++ src/main/java/com/itch/fmp/App.java
@@ -12,6 +12,6 @@ public class App
     public static void main( String[] args )
     {		
 		logger.info("Hello World");
-        System.out.println( "Hello World!" );
+		logger.info("Tharindu Chandika Malimage");
     }
 }
